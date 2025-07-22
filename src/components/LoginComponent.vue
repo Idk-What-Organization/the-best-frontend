76b4ca1be9e4802b22cb21810a0b3b1a4b75c016
ForagiFox <244107023004@student.polinema.ac.id>
2025-07-22 23:14:42 +0700

    Login ah moment

---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1 class="text-4xl font-bold mb-1">LOGO</h1>
+  <p class="mb-5">Login to your account</p>
+  <form action="#" class="mb-5">
+    <input type="text" placeholder="Username" class="input input-bordered w-full mb-2 border-sm" />
+    <input
+      type="password"
+      placeholder="Password"
+      class="input input-bordered w-full mb-2 border-sm"
+    />
+    <button type="submit" class="btn btn-primary w-full">Login</button>
+  </form>
+  <div class="flex items-center gap-2 mb-5">
+    <hr class="flex-1 border-gray-400" />
+    <span class="text-gray-600">Or</span>
+    <hr class="flex-1 border-gray-400" />
+  </div>
+  <div class="flex items-center justify-center gap-2">
+    <p>Don't have an account?</p>
+    <a href="#" class="text-primary ml-1 font-semibold" @click.prevent="$emit('toggle-auth')"
+      >Register now</a
+    >
+  </div>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { useRoute, useRouter } from 'vue-router'
+import { computed } from 'vue'
+import LoginComponent from '@/components/LoginComponent.vue'
+import RegisterComponent from '@/components/RegisterComponent.vue'
+
+const route = useRoute()
+const router = useRouter()
+
+const mode = computed<'login' | 'register'>(() =>
+  route.name === 'Register' ? 'register' : 'login',
+)
+
+function toggleRoute() {
+  if (mode.value === 'login') router.push({ name: 'Register' })
+  else router.push({ name: 'Login' })
+}
+</script>
+
+<template>
+  <div class="flex h-screen items-center justify-center">
+    <div
+      class="bg-white rounded-lg shadow-lg flex overflow-hidden items-center justify-center gap-4"
+    >
+      <div>
+        <img
+          src="@/assets/842746760e7e0bcf832ec9bdb4a7887f1.png"
+          alt="image"
+          class="w-full h-auto object-cover"
+        />
+      </div>
+      <div class="mr-5 relative overflow-hidden w-md">
+        <component :is="mode === 'login' ? LoginComponent : RegisterComponent" @toggle-auth="toggleRoute" />
+      </div>
+    </div>
+  </div>
+</template>
